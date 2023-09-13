bedd20ff6f8011203da024406f783da5c746a737
Marco Pizzari <marco.pizzari94@gmail.com>
2023-09-13 11:52:34 +0200

    ciao

---
+++ resources/views/article/create.blade.php
@@ -0,0 +1,59 @@
+<x-layout>
+
+<div class="container">
+    <h2 class="my-4">Inserisci il nuovo articolo</h2>
+    <div class="row">
+            @if ($errors->any())
+            <div class = "alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+                @endforeach
+            </ul>
+            </div>
+            @endif
+
+
+        <form action="{{ route('article.store') }}"
+            method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="d-flex justify-content-between">
+                <div class="col-12 col-md-6">
+                    <div class="mb-3 mx-5">
+                        <label class="form-label">Titolo</label>
+                        <input type="text" class="form-control" name="title" >
+                        
+                    </div>
+                    <div class="mb-3 mx-5">
+                        <label class="form-label">Sottotitolo</label>
+                        <input type="text" class="form-control" name="subtitle" >
+                       
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6">
+                    <div class="mb-3 mx-5">
+                        <label class="form-label">Immagine</label>
+                        <input type="file" class="form-control" name="image" >
+                       
+                    </div>
+                    <div class="mb-3 mx-5">
+                        <label class="form-label">Categorie</label>
+                        <select class="form-control text-capitalize" name="category" >
+                       @foreach($categories as $category)
+                       <option value="{{$category->id}}">{{$category->name}}</option>
+                       @endforeach
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Corpo del testo</label>
+                <textarea class="form-control" name="body" cols="30" rows="10"></textarea>
+          
+            </div>
+            <button type="submit" class="btn btn-primary">Inserisci un articolo</button>
+        </form>
+    </div>
+</div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
+use App\Http\Controllers\ArticleController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [PublicController::class, 'home'])->name('home');
+Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
+Route::post('/article/store', [ArticleController::class, 'store'])->name('article.store');
+
+
