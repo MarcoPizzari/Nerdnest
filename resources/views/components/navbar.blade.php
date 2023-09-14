38cc7870fc8ac89e2132bfeb34ef2d62b88edf3b
Marco Pizzari <marco.pizzari94@gmail.com>
2023-09-14 13:14:58 +0200

    modifiche logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">progetto finale</a>
+    <div class="logo">
+      
+    </div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -13,7 +15,7 @@
           <a class="nav-link active" aria-current="page" href="{{route('article.index')}}">Tutti gli articoli</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{route('article.create')}}">Inserisci un articolo</a>
+          <a class="nav-link text-dark" href="{{route('article.create')}}">Inserisci un articolo</a>
         </li>
         @auth
         <li class="nav-item">
