11650f0a648796fdf55cbd308e14717ca6fb9cb1
Ludovico Paris <ludovicoparis1997@gmail.com>
2023-09-20 18:40:21 +0200

    bsckground

---
+++ resources/views/components/background.blade.php
@@ -0,0 +1,16 @@
+<main>
+
+    <!-- This div is  intentionally blank. It creates the transparent black overlay over the video which you can modify in the CSS -->
+    <div class="overlay"></div>
+
+    <!-- The HTML5 video element that will create the background video on the main -->
+    <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="./../video/main.mp4" type="video/mp4">
+    </video>
+
+
+<div class="  ">
+    {{ $slot }}
+</div>
+
+</main>
